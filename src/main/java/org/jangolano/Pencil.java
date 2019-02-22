80db0076783a9fc7d423c767127ecdd78734b654
Josh Angolano <grangolano@gmail.com>
2019-02-22 07:12:45 -0600

    Added Failing Test Use Eraser

--- src/main/java/org/jangolano/Pencil.java
+++ src/main/java/org/jangolano/Pencil.java
@@ -1,18 +1,31 @@
 package org.jangolano;
 
+import java.util.ArrayList;
+
 public class Pencil {
 
     int point;
     int length;
-
+    int eraser;
     Pencil(){
         point = 500;
         length = 20;
+        eraser = 500;
     }
 
     public String write(String text){
-        point-=text.length();
-        return text;
+        char [] textToBeWritten = text.toCharArray();
+        StringBuffer textToWrite = new StringBuffer();
+
+        for(int i=0;i<textToBeWritten.length;i++){
+            if(point>0) {
+                point-=1;
+                textToWrite.append(textToBeWritten[i]);
+            }else{
+                break;
+            }
+        }
+        return textToWrite.toString();
     }
 
     public int getPoint(){
@@ -30,5 +43,13 @@ public class Pencil {
 
     public int getLength(){
         return length;
+    }
+
+    public int getEraser(){
+        return eraser;
+    }
+
+    public void erase(String text){
+
     }
 }
--- src/test/java/org/jangolano/PencilTest.java
+++ src/test/java/org/jangolano/PencilTest.java
@@ -49,6 +49,13 @@ public class PencilTest {
        Pencil pencil = new Pencil();
        assertEquals(pencil.write("Rp4TptSAfWIODaWai9r2JX15MqDsSw5YeQd92O5qtffHMAZUDQv3UHfW8RN5Eh4r05JIYLxkX6nQfpioG530nZklYX69LZjUayI56m3Hdfo2BG6tfvadJm16E9LQmnlGxIlevzUXOgiCrsqEGc2hydU6R5UhQjTW1R2ajVjff4gzZTmy2XM7rGWeGk4vZmhT3pPDkEfMzMHJxJOcp4tpOSN34Kybzy15zJf7V77TXHIwvkKzlplPN4ZBZlJ4eNYMMUNN4jxi0Zn7qRSraKmRFw9Ihnc5BjpYLwpziOFqnr0v7lEjThYa3mUjTvlDjfMzF4diwY1j4utZj8Pkr2CGrWSQGuGC5D778RIQJzVTxXqt4IByizB3e40thB1noDjsdQKnDj90V1UCrQBO10jK9zK8TiA7HHe5oWUF8ye5uCfE2LVKNzatzIN8PoCJ4gyQxsdx7XOB6BWSaxQHYfa6tNwaL5s49lXlWX6fHtWPDabOipgu7kztxj2oit1ALgkbZOutDLP7F2lle"),
                "Rp4TptSAfWIODaWai9r2JX15MqDsSw5YeQd92O5qtffHMAZUDQv3UHfW8RN5Eh4r05JIYLxkX6nQfpioG530nZklYX69LZjUayI56m3Hdfo2BG6tfvadJm16E9LQmnlGxIlevzUXOgiCrsqEGc2hydU6R5UhQjTW1R2ajVjff4gzZTmy2XM7rGWeGk4vZmhT3pPDkEfMzMHJxJOcp4tpOSN34Kybzy15zJf7V77TXHIwvkKzlplPN4ZBZlJ4eNYMMUNN4jxi0Zn7qRSraKmRFw9Ihnc5BjpYLwpziOFqnr0v7lEjThYa3mUjTvlDjfMzF4diwY1j4utZj8Pkr2CGrWSQGuGC5D778RIQJzVTxXqt4IByizB3e40thB1noDjsdQKnDj90V1UCrQBO10jK9zK8TiA7HHe5oWUF8ye5uCfE2LVKNzatzIN8PoCJ4gyQxsdx7XOB6BWSaxQHYfa6tNwaL5s49lXlWX6fHtWPDabOipgu7kzt");
+    }
 
+    @Test
+    public void testUseEraser(){
+        Pencil pencil = new Pencil();
+        pencil.write("ABCDEFG");
+        pencil.erase("ABC");
+        assertEquals(pencil.getEraser(), 497);
     }
 }
